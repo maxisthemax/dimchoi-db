b04b08870e5a169d11554d06859e04846330e0b5
maxisthemax <maxisthemax89@gmail.com>
2017-11-14 20:52:56 +0800

    add UI for view table and order status

--- development/resorder.php
+++ development/resorder.php
@@ -23,18 +23,21 @@ function getresorder($resid) {
     $uri = $_SERVER['REQUEST_URI']; // holds url for last page visited.s
     global $dbhandler0;
     
-    $sqlresorder = "SELECT *
-    FROM resorder a LEFT JOIN qrcode_type b on a.i_resorder_type_id = b.i_qr_type_id
+    $sqlresorder = "SELECT *,b.va_res_order_status
+    FROM resorder a LEFT JOIN qrcode_type c on a.i_resorder_type_id = c.i_qr_type_id
+    LEFT JOIN resorderstatus b on a.i_status = b.i_res_order_status
     WHERE a.i_res_id = $resid";
     $resresorder = $dbhandler0->query($sqlresorder);
 
     echo'<form action="index.php" method="post"><table id="tab" width=100%  border=1 style="font-size:100%;">';
     echo "<tr><TD colspan = 3 align=center>Res Order CODE</td></tr>";    
-    echo "<tr><td width=10%>Res Order ID</td><td width=10%>Res Order Type</td><td width=10%>Res Order ID</td><td>Res Order Data 1</td><td>Res Order Data 2</td><td>Order Create Date</td></tr>";
-
+    echo "<tr><td width=10%>Res Order ID</td><td width=10%>Res Order Type</td><td width=10%>Res Order ID</td><td>Res Order Data 1</td><td>Res Order Data 2</td><td>Order Create Date</td>
+    <td>Order Table No</td><td>Order Status</td><td>Order Closed Date</td></tr>";
     foreach($resresorder as $resvalue) 
     {
-        echo "<tr><td>".$resvalue['i_resorder_id']."</td><td>".$resvalue['va_qr_type_name']."</td><td>".$resvalue['i_user_id']."</td><td>".$resvalue['va_resorder_data_1']."</td><td>".$resvalue['va_resorder_data_2']."</td><td>".$resvalue['dt_resordercreate']."</td></tr>";
+        echo "<tr><td>".$resvalue['i_resorder_id']."</td><td>".$resvalue['va_qr_type_name']."</td><td>".$resvalue['i_user_id']."</td><td>".$resvalue['va_resorder_data_1']."</td><td>"
+        .$resvalue['va_resorder_data_2']."</td><td>".$resvalue['dt_resordercreate']."</td><td>".$resvalue['i_res_order_table_id']."</td><td>".$resvalue['va_res_order_status']."</td><td>"
+        .$resvalue['dt_resorderclosed']."</td></tr>";
     }   
     echo '</form>';       
     } 
