6a8515cba00bbc40e4a04480197d58cd8bf17cf3
maxisthemax <maxisthemax89@gmail.com>
2017-12-22 15:10:11 +0800

    fix

--- query/insert/func.php
+++ query/insert/func.php
@@ -229,14 +229,14 @@ function insertorderfromqr() {
         $sqlinstouserorder = 
         "INSERT INTO 
         userorder (i_userorder_id,i_res_id,i_user_id,i_userorder_type_id,va_userorder_data_1,va_userorder_data_2,dt_create,i_status,dt_userordercreate,dt_userorderclosed,i_user_order_table_id) 
-        values ('$qr_id','$res_id','$user_id','$qr_type_id','$jsondata','$qr_data_2','$create_date',1,now(),'0000-00-00 00:00:00','$res_order_table')";
+        values ('$qr_id','$res_id','$user_id','$qr_type_id','$jsondata','[]','$create_date',1,now(),'0000-00-00 00:00:00','$res_order_table')";
 
         $resinsuseroder = $dbhandler0->insert($sqlinstouserorder,1);
 
         $sqlinstoresorder = 
         "INSERT INTO 
         resorder (i_resorder_id,i_res_id,i_user_id,i_resorder_type_id,va_resorder_data_1,va_resorder_data_2,dt_create,i_status,dt_resordercreate,dt_resorderclosed,i_res_order_table_id) 
-        values ('$qr_id','$res_id','$user_id','$qr_type_id','$jsondata','$qr_data_2','$create_date',1,now(),'0000-00-00 00:00:00','$res_order_table')";
+        values ('$qr_id','$res_id','$user_id','$qr_type_id','$jsondata','[]','$create_date',1,now(),'0000-00-00 00:00:00','$res_order_table')";
 
         $resinsresorder = $dbhandler0->insert($sqlinstoresorder,1);    
 
